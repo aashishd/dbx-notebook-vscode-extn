35a91f178d67c928ccdbe5d5037c2a3636405310
aashishd <ashish.dhiman.nith@gmail.com>
2025-11-21 22:02:48 +0000

    allows serializing and deserializing %md and code cells correctly

--- databricks-notebooks-editor/src/extension.ts
+++ databricks-notebooks-editor/src/extension.ts
@@ -2,6 +2,11 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const DATABRICKS_NOTEBOOK_HEADER = '// Databricks notebook source';
+const MARKDOWN_PREFIX = '// MAGIC';
+const MARKDOWN_CELL_HEADER = `${MARKDOWN_PREFIX} %md`;
+const CELL_SEPARATOR = '// COMMAND ----------';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,20 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "databricks-notebooks-editor" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('databricks-notebooks-editor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Databricks Notebooks Editor!');
-	});
-
-	const notebookSerializer = vscode.workspace.registerNotebookSerializer('my-notebook', new SampleSerializer());
-
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(notebookSerializer);
+	context.subscriptions.push(vscode.workspace.registerNotebookSerializer('my-notebook', new SampleSerializer()));
 }
 
 interface DatabricksCell {
@@ -39,7 +31,7 @@ class SampleSerializer implements vscode.NotebookSerializer {
     const contents = new TextDecoder().decode(content);
     
     // Parse Databricks source format
-    const databricksCells = this.parseDatabricksSource(contents);
+    const databricksCells: DatabricksCell[] = this.parseDatabricksSource(contents);
     
     const cells = databricksCells.map(
       item =>
@@ -56,42 +48,40 @@ class SampleSerializer implements vscode.NotebookSerializer {
   }
 
   private parseDatabricksSource(content: string): DatabricksCell[] {
+    // ignore the first line as it is always going to be `// Databricks notebook source`
+    const lines = content.split('\n');
+    // Remove the first line (Databricks notebook source)
+    const contentWithoutHeader = lines.slice(1).join('\n');
     const cells: DatabricksCell[] = [];
-    const cellSeparator = '// COMMAND ----------';
-    const magicPrefix = '// MAGIC';
     
     // Split by cell separator
-    const rawCells = content.split(cellSeparator);
-    
+    const rawCells = contentWithoutHeader.split(CELL_SEPARATOR);
+
     for (const rawCell of rawCells) {
       const trimmedCell = rawCell.trim();
       if (!trimmedCell) {
         continue;
       }
+
+      let isMarkdown = trimmedCell.startsWith(MARKDOWN_CELL_HEADER);
       
       const lines = trimmedCell.split('\n');
+      if(isMarkdown){
+        // Remove the first line which contains the %md magic
+        lines.shift();
+      }
       let cellContent: string[] = [];
-      let isMarkdown = false;
       
       // Check if this is a markdown cell (starts with // MAGIC)
       for (const line of lines) {
         const trimmedLine = line.trim();
-        
-        if (trimmedLine.startsWith(magicPrefix)) {
-          isMarkdown = true;
-          // Remove the // MAGIC prefix and keep the rest
-          const contentAfterMagic = line.substring(line.indexOf(magicPrefix) + magicPrefix.length);
-          // Remove leading space if present
-          cellContent.push(contentAfterMagic.startsWith(' ') ? contentAfterMagic.substring(1) : contentAfterMagic);
-        } else if (trimmedLine.length > 0) {
-          // Regular code line
-          cellContent.push(line);
-        } else if (cellContent.length > 0) {
-          // Preserve empty lines within content
-          cellContent.push(line);
+        if (isMarkdown){
+            cellContent.push(this.removeMagicPrefix(trimmedLine, MARKDOWN_PREFIX));
+        }
+        else{
+            cellContent.push(trimmedLine);
         }
       }
-      
       if (cellContent.length > 0) {
         cells.push({
           content: cellContent.join('\n').trim(),
@@ -99,9 +89,17 @@ class SampleSerializer implements vscode.NotebookSerializer {
           language: 'scala'
         });
       }
-    }
+  }
     
     return cells;
+  }
+
+  private removeMagicPrefix(line: string, magicPrefix: string): string {
+    if (line.trim().startsWith(magicPrefix)) {
+      const contentAfterMagic = line.substring(line.indexOf(magicPrefix) + magicPrefix.length);
+      return contentAfterMagic.startsWith(' ') ? contentAfterMagic.substring(1) : contentAfterMagic;
+    }
+    return line;
   }
 
   async serializeNotebook(
@@ -113,24 +111,26 @@ class SampleSerializer implements vscode.NotebookSerializer {
   }
 
   private serializeToDatabricksFormat(data: vscode.NotebookData): string {
-    const cellSeparator = '// COMMAND ----------';
-    const magicPrefix = '// MAGIC';
     const output: string[] = [];
+    // Add header
+    output.push(DATABRICKS_NOTEBOOK_HEADER);
+    output.push('');
 
     for (let i = 0; i < data.cells.length; i++) {
       const cell = data.cells[i];
       
       // Add cell separator (except before the first cell)
       if (i > 0 || output.length > 0) {
-        output.push(cellSeparator);
+        output.push(CELL_SEPARATOR);
         output.push('');
       }
       
       if (cell.kind === vscode.NotebookCellKind.Markup) {
         // Markdown cell - prefix each line with // MAGIC
+        output.push(MARKDOWN_CELL_HEADER)
         const lines = cell.value.split(/\r?\n/);
         for (const line of lines) {
-          output.push(`${magicPrefix} ${line}`);
+          output.push(`${MARKDOWN_PREFIX} ${line}`);
         }
       } else {
         // Code cell - output as-is
